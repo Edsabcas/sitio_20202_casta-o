f9b638d84e8e117375210bb94d7b841ff0658fe2
edgsabcas <edgar.casuy123@gmail.com>
2022-05-25 00:11:49 -0600

    cambios

--- app/Http/Controllers/MensajesController.php
+++ app/Http/Controllers/MensajesController.php
@@ -11,14 +11,14 @@ class MensajesController extends Controller
 
 
    public function mensaje(){
-       $sid='ACf65a4d05035612081d718a163f8846ef';
-       $tok="feba3aaea96f657b8130e5229504a221";
+    //   $sid='ACf65a4d05035612081d718a163f8846ef';
+      // $tok="6dc2ddb7908517da6679d626b5b16b58";
        $twilio = new Client( $sid, $tok );
        $message = $twilio->messages
     ->create("whatsapp:+50250549056", // to
              [
                  "from" => "whatsapp:+14155238886",
-                 "body" => "Hola **Amigos**,!"
+                 "body" => "Hola *Amigos*,!"
              ]
     );
 
