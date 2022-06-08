c1eadae554c9ff8605e1c8ea62f47c94819a7217
Jimena Gómez <jimenalejandra0292@gmail.com>
2022-06-08 10:56:19 -0600

    diseño Foother

--- resources/views/plantilla.blade.php
+++ resources/views/plantilla.blade.php
@@ -278,12 +278,7 @@
    @livewireScripts
    </body>
    <footer>
-<<<<<<< HEAD
     <div class="footer" style="background-color: #272b61">
-=======
-
-    <div class="footer" style="background-color:#4f4f52">
->>>>>>> eb17255f4f7dd0d1e418332fdb3213827a31195b
        <div class="container">
          <div class="row">
             <div class="col text-center">
@@ -318,33 +313,10 @@
                   <span>Lunes - Viernes</span><br>
                   <span><svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-alarm-fill" viewBox="0 0 16 16">
                      <path d="M6 .5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H9v1.07a7.001 7.001 0 0 1 3.274 12.474l.601.602a.5.5 0 0 1-.707.708l-.746-.746A6.97 6.97 0 0 1 8 16a6.97 6.97 0 0 1-3.422-.892l-.746.746a.5.5 0 0 1-.707-.708l.602-.602A7.001 7.001 0 0 1 7 2.07V1h-.5A.5.5 0 0 1 6 .5zm2.5 5a.5.5 0 0 0-1 0v3.362l-1.429 2.38a.5.5 0 1 0 .858.515l1.5-2.5A.5.5 0 0 0 8.5 9V5.5zM.86 5.387A2.5 2.5 0 1 1 4.387 1.86 8.035 8.035 0 0 0 .86 5.387zM11.613 1.86a2.5 2.5 0 1 1 3.527 3.527 8.035 8.035 0 0 0-3.527-3.527z"/>
-<<<<<<< HEAD
                   </svg> 7:00 a.m - 14:00 p.m.</span>
                </div>
             </div>
          </div>
-=======
-                   </svg> 7:00 a.m - 14:00 p.m.</span>
-             </div>
-          </div>
-
-       </div>
-      </div>
- 
-       <div class="contact text-center">
-
-          <h3 class="text-xs text-light">Ubicación</h3>
-        <p style="color:white"><svg xmlns="http://www.w3.org/2000/svg" width="10" height="10" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
-         <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
-       </svg> 0 av b 1-45 zona 4 Montserrat 1 01057-Mixco, Guatemala.</p><br>
-        <p>
-         <div class="map-responsive">
-
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3860.0995604467653!2d-90.5839736856792!3d14.650289479785926!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8589a0226e5847b5%3A0x97abf4d970bd6fac!2sColegio%20Biling%C3%BCe%20El%20Casta%C3%B1o!5e0!3m2!1ses!2sgt!4v1619627593600!5m2!1ses!2sgt" width="300" height="200" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
-        
-         </div>
-        </p>
->>>>>>> eb17255f4f7dd0d1e418332fdb3213827a31195b
       </div>
 
        <div class="copyright">
