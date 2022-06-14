23b11fa1c1b82f0c20b9f0575d2407ecb17d1d50
Christopher Navas <christophernavas2004@gmail.com>
2022-06-13 22:10:22 -0600

    Cambios

--- resources/views/estados/info5.blade.php
+++ resources/views/estados/info5.blade.php
@@ -203,16 +203,6 @@
                       <label for="exampleInputPassword1" class="form-label " style="font-size:20px">Adjunte un archivo </label>
                       <input type="file" class="form-control " wire:model='archivo'  style="border:2px solid rgba(86, 95, 76, 0.466);" id="exampleInputPassword1">
                        <div class="col-sm-10">
-                        @if($formato==1)
-                        <h3 class="form-label">Visualización de Imagen</h3>
-                        <img src="{{$archivo->temporaryURL()}}" height="200" weight="200"  alt="...">
-                        @endif
-                        @if($formato==2)
-                        <h3 class="form-label">Visualización de Video</h3>
-                        <video height="500" weight="500" class="card-img-top" alt="..." controls>
-                          <source src="{{$archivo->temporaryURL()}}"  type="video/mp4">
-                        </video>
-                        @endif
                         @if($formato==3)
                         <h3 class="form-label">Visualización de PDF</h3>
                           <iframe width="400" height="400" src="/images/temporalpdf/{{$arch}}" frameborder="0"></iframe>
