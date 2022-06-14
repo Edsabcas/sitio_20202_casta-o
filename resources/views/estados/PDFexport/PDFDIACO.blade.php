e449172e9261cbc212d2f2455140ea02db3ae062
unknown <ronia3606@gmail.com>
2022-06-14 00:01:06 -0600

    cambios

--- resources/views/estados/PDFexport/PDFDIACO.blade.php
+++ resources/views/estados/PDFexport/PDFDIACO.blade.php
@@ -7,23 +7,140 @@
     <title>Document</title>
 </head>
 <body>
-    <div>
-        <h1>Documento Importante</h1>
-    </div>
-    <div>
-        
-        <table>
-            <thead>
-                <th>
-                    <td>Nombre de Estudiante</td>
-                </th>
-            </thead>
+        <p>CA-83-2016</p>
+        <strong><p>CONTRATO DE ADHESION POR PRESTACION DE SERVICIOS EDUCATIVOS
+            COLEGIO BILINGÜE "EL CASTAÑO"</p></strong>
+        <p>Correlativo interno Contrato No. _____________ </p>
+        <p> <strong>Aprobado y registrado según Resolución DIACO: _____________</strong> </p>
+            <hr>
+        <p>En el municipio de Mixco, del departamento de Guatemala, el día __________ del mes de _____________________ 
+            del año dos mil ____________. NOSOTROS: Virginia Yomara Gonzalez Calderon de Siliezar, de____________
+            años de edad, Casada, guatemalteca, Licenciada en Administación Educativa, de este domicilio, que me identifico 
+            con Documento Personal de Identificción, Código Único de Identificación número 2617 37260 0101 extendido por el 
+            Registro Nacional de las Personas de la República de Guatemala, actuando en mi calidad de Propietaria del centro 
+            educativo denominado COLEGIO BILINGÜE "EL CASTAÑO", ubicado en 0 Avenida "B" 1-45 Zona 4, Montserrat I, de 
+            este Municipio lo que acredito con Resolución: No. 005-95-EP., de fecha 9 de enero de 1995, emitida por Direcciòn 
+            Tecnica Regional Metropolitana, . Y por la otra parte: </p>
+        <p>(DATOS DEL PADRE DE FAMILIA O REPRESENTANTE LEGAL DEL EDUCANDO)
+            ___________________________________________________________________________________________ 
+             (nombres) (apellidos) 
+            de __________________________, _________________________________, _____________________________,
+             (años de edad) (nacionalidad) (estado civil) 
+            ____________________________, de este domicilio, que me identifico con el ______________________________ 
+             (profesión u oficio) 
+            _____________________________________________________________________________________________ 
+             (Documento Personal de Identificación (DPI) o Pasaporte)
+            extendido por _____________________________________________________, con residencia en: 
+            _____________________________________________________________________________________________, 
+            con número de teléfono de casa ____________________________, oficina _____________________________ y 
+            celular ________________________, correo electrónico ___________________________________________, 
+            actuando en mi calidad de _________________________________ (en adelante relacionado como “el 
+            Representante del educando”) declarando que la información personal proporcionada es de carácter confidencial. 
+            Los comparecientes aseguramos ser de los datos de identificación anotados, estar en el libre ejercicio de nuestros 
+            derechos civiles y celebramos CONTRATO DE ADHESION POR PRESTACION DE SERVICIOS EDUCATIVOS, de 
+            conformidad con las siguientes cláusulas:
             
-            <tbody>
-                <td>Hola</td>
-            </tbody>
-        </table>
-        
-    </div>
-</body>
+        </p>
+        <strong><p>PRIMERA. INFORMACION DEL EDUCANDO Y SERVICIO EDUCATIVO CONTRATADO</p></strong>
+        <p>_____________________________________________________________________________________________ 
+            (nombres) (apellidos)
+            quien cursará el __________________________ grado, del nivel y/o carrera ______________________________, 
+            jornada matutina, plan diario. Servicio educativo autorizado por el Ministerio de Educación según Resolución(es) No. 
+            005-95-EP. Con fecha 9 de enero de 1995, emitida por la Direcciòn Tecnica Regional Metropolitana; No. 0204-99-
+            E.P. con fecha 13 de marzo de 1999 emitida por la Direcciòn Departamental de Guatemala, misma(s) que se tiene(n) 
+            a la vista.</p>
+        <p> <strong><p>SEGUNDA. VOLUNTARIEDAD EN LA CONTRATACION DEL SERVICIO.</p></strong> Manifiesta el Representante del 
+            Educando, que conociendo la amplia oferta de instituciones privadas que prestan servicios educativos, de manera 
+            voluntaria y espontánea ha elegido al Centro Educativo para la educación académica del educando, pues a su juicio 
+            cumple con todos y cada uno de los requerimientos que necesita satisfacer para el efecto. Por tal razón el 
+            responsable del educando se obliga a cumplir la totalidad de las obligaciones contraídas en el presente Contrato bajo 
+            el entendido que de generarse cualquier incumplimiento, no solo por el Responsable del Educando sino además del 
+            Centro Educativo, genera las consecuencias indicadas en el presente Contrato, así como las determinadas por la 
+            Legislación vigente.
+            </p>
+        <p> <strong><p>TERCERA. DEL PLAZO.</p></strong> TERCERA. DEL PLAZO. El servicio educativo convenido en este contrato será válido para el ciclo escolar del año 
+            dos mil ______________, durante su vigencia no puede ser modificada ninguna de sus cláusulas, las que se 
+            deberán cumplir a cabalidad; así como con todo lo ofrecido a los padres de familia, tanto en la publicidad efectuada 
+            en los medios de comunicación, información escrita o cualquier otro documento publicitario. El representante del 
+            Educando y el Centro Educativo podrán suscribir un Nuevo Contrato para el ciclo escolar inmediato siguiente, en 
+            caso acuerden la continuidad del educando en el Centro Educativo.</p>
+        <p> <strong><p>CUARTA. DERECHOS DEL EDUCANDO, PADRE DE FAMILIA O REPRESENTANTE LEGAL.</p></strong> Como usuarios del servicio educativo contratado en armonía con el Artículo 4 de la Ley de Protección al Consumidor y Usuario, tendrá 
+            derecho a:</p>
+
+        <p>A. <strong><p>La protección a la vida, salud y seguridad en la adquisición, consumo y uso de bienes y servicios:</p></strong>  Las 
+            instalaciones del centro educativo, están adecuadas para que los educandos no corran ninguna clase de 
+            riesgo que ponga en peligro su integridad física, siempre y cuando haga uso adecuado de las mismas, también 
+            están dotadas de todos los servicios básicos ofrecidos a los padres de familia. En el caso que la prestación del 
+            servicio de transporte sea brindado por una entidad ajena al centro educativo, este proporcionará todas las 
+            medidas de seguridad necesarias para la debida protección de los educandos, liberando a su vez al Centro 
+            escolar de cualquier responsabilidad de cualquier índole que se le pretendiera imputar en tal sentido; así mismo, 
+            propondrán medidas de seguridad a los padres a través de un seguro escolar, el cual no será obligatorio, pero 
+            debe quedar en acta o constancia escrita de la decisión del responsable del educando de la adquisición o no del 
+            mismo, asumiendo éste cualquier responsabilidad que se pudiera generar por la decisión de no contratar el 
+            mismo. Se debe de velar por la seguridad de los educandos, en cualquier actividad propuesta y hacerlo siempre 
+            del conocimiento del Responsable del Educando, para su respectivo consentimiento de participación.
+            </p>
+        <p>B. <strong><p>La libertad de elección del bien o servicio:</p></strong> Los padres de familia tienen el derecho de poder adquirir, tanto los 
+            útiles escolares, como los uniformes, transporte, seguro y otros servicios adicionales, en el establecimiento 
+            comercial que se adecúe mejor a su capacidad económica; sin embargo, el centro educativo puede facilitar la 
+            compra o prestación de servicios, siempre y cuando medie convenio por escrito, en el cual lo solicitan los padres 
+            de familia, debiendo en tal caso, cumplir con las obligaciones tributarias correspondientes.
+            Respecto a los seguros de gastos médicos y cualesquiera otros seguros que el padre de familia decida 
+            contratar, el Centro Educativo solicita como mínimo que estos cubran accidentes y traslados en ambulancia a 
+            centros asistenciales.
+            En caso el Responsable del Educando decida adquirir los bienes indicados en la presente cláusula a 
+            proveedores distintos del Centro Educativo, los mismos deben de ser aceptados siempre y cuando tengan las 
+            características necesarias para su utilización, con el objeto que dicha igualdad de insumos asegure una inclusión 
+            total del educando y se facilite el trabajo académico tanto del educando como de los docentes del Centro 
+            Educativo.
+            </p>
+        <p>C. <strong><p>La libertad de contratación:</p></strong> El representante del educando, tiene el derecho a la libre contratación iniciando 
+            por la elección del Centro Educativo en el que ha decidido inscribir al Educando, aceptando que dicho Centro 
+            Educativo es la mejor opción que se adecúa a sus requerimientos educativos y capacidades económicas. En 
+            igual sentido, puede contratar o adquirir los bienes o servicios (Transporte, Seguro Escolar y otros), que más se 
+            adecúen a su capacidad económica, siempre y cuando cumplan en su totalidad con las autorizaciones y 
+            requerimientos operativos, de seguridad y de cualquier otra índole requeridos por la legislación vigente, 
+            debiendo para el efecto informar al Centro Educativo de tal contratación, previo al inicio del ciclo escolar. </p>
+        <p>D. <strong><p>La información veraz, suficiente, clara y oportuna sobre los bienes y servicios:</p></strong>  El centro educativo se 
+            compromete a proporcionar al Responsable del Educando, la información completa sobre el servicio contratado 
+            y especialmente los horarios de clases, los grados y las carreras autorizadas que se imparten, los sistemas de 
+            evaluación, reglamentos, manuales y políticas, que se fundamente con las disposiciones legales y 
+            reglamentarias vigentes, cursos adicionales que imparten, el monto de las cuotas que cobran, tanto de 
+            inscripción como de cuota mensual; así como, de las actividades extraescolares de carácter voluntario u 
+            optativas, que se puedan realizar durante el ciclo escolar respectivo. Cuando el responsable del educando esté 
+            de acuerdo en contratar los servicios antes indicados manifiesta de manera expresa, espontánea y voluntaria 
+            cumplir en su totalidad y comprometerse a que el Educando también los cumpla, aceptando a su vez las 
+            consecuencias que genere cualquier incumplimiento a los mismos, siempre y cuando dichas consecuencias no 
+            menoscaben el derecho a la educación que le asiste al educando. Las autoridades del centro educativo, tienen 
+            la obligación de cumplir con las leyes y Acuerdos Ministeriales aplicables a su actividad.
+            </p>
+        <p>E. <strong><p>Utilizar el Libro de Quejas o el medio legalmente autorizado por la Dirección de Atención y Asistencia al 
+            Consumidor, para dejar registro de su disconformidad con respecto a un bien adquirido o servicio 
+            contratado:</p></strong>  Al hacer constar su inconformidad en el libro de quejas el firmante debe esperar un período de ocho 
+            días para que la misma sea resuelta por las autoridades del centro educativo, transcurrido ese tiempo sin que 
+            exista una solución satisfactoria deberá interponer la queja correspondiente ante la DIACO quien procederá 
+            según corresponda</p>
+        <p><strong><p>QUINTA. DEL DERECHO DE RETRACTO.</p></strong>El padre de familia o representante legal del educando tendrá derecho a 
+        retractarse dentro de un plazo no mayor de cinco días hábiles contados a partir de la firma del contrato, siempre que 
+        no hubiere hecho uso del bien o servicio. Si ejercita oportunamente este derecho, le serán restituidos en su totalidad 
+        los valores pagados.</p>
+        <p><strong><p>SEXTA. OBLIGACIONES DEL PADRE DE FAMILIA O REPRESENTANTE LEGAL DEL EDUCANDO.</p></strong>En armonía 
+        con el Artículo 5 de la Ley de Protección al Consumidor y Usuario, tendrá las siguientes obligaciones:
+        </p>
+        <br>
+        <p>A. Pagar por los bienes o servicios en el tiempo, modo y condiciones establecidas mediante el presente contrato.
+        </p>
+        <p>B. Utilizar los bienes y servicios en observancia a su uso normal, de conformidad con las especificaciones 
+            proporcionadas por el proveedor y cumplir con las condiciones pactadas, debiendo para tal efecto instruir al 
+            educando sobre el cuidado tanto de las instalaciones, como del mobiliario y equipo del centro educativo. En caso 
+            de daños y perjuicios ocasionados por el alumno, el padre de familia será el responsable, siempre y cuando 
+            sean debidamente comprobados y atribuidos al mismo, de conformidad con lo establecido en el artículo 1645 del 
+            Código Civil (Decreto Ley Número 106) o cualquier otra disposición legal aplicable que se encuentre vigente al 
+            momento de generarse los mismos.</p>
+        <p>C. Cumplir en su totalidad las estipulaciones contenidas en el presente Contrato, así como los reglamentos y 
+            demás disposiciones internas establecidas por el Centro Educativo, en tanto cumplan con las disposiciones 
+            legales y reglamentarias vigentes, pues en caso contrario serán nulas y no obligarán al Representante del 
+            Educando.
+            </p>
+        </body>
 </html>
