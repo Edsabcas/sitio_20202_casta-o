61d1d69d6e1bdc30280b9c405fda7910ecde8592
Camila Guevara <camilaguevara277@gmail.com>
2022-06-17 00:27:15 -0600

    linea 3

--- resources/views/estados/forms/info3_form.blade.php
+++ resources/views/estados/forms/info3_form.blade.php
@@ -2,14 +2,19 @@
 <div wire:ignore.self class="modal fade" id="ModalInscripcion" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
   <div class="modal-dialog modal-xl">
     <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Datos importantes</h5>
+      <div class="modal-header" style="background:#a4cb39;color:rgb(255, 255, 255)">
+        <h5 class="modal-title" id="exampleModalLabel" style="color:#ffffff;"><strong><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-triangle-fill" viewBox="0 0 16 16">
+          <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+        </svg>  Datos importantes</strong></h5>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
+        </div>
+
+        
+      
       <div class="modal-body">
           <!-- FORM DENTRO DE LA MODAL -->
           <h2 class="card-title" style="color:#3a3e7b;" >
-              Ingrese la siguiente información importante para continuar con su proceso de inscripción</h2>
+              Ingrese la siguiente información para continuar con su proceso de inscripción</h2>
             <form action="" wire:submit.prevent='' enctype="multipart/form-data" method="POST">
              <div wire:ignore.self class="accordion" id="accordionDatosVarios">
               <div style="border-radius: 60px 60px 60px 60px;" class="accordion-item">
@@ -1008,6 +1013,7 @@
         <button type="button" class="btn btn-pre2" wire:click='insertar_datos()' data-bs-dismiss="modal">Envíar Información</button>.
       </div>
     </div>
+  </div>
   </div>
 </div>
 </div>
